2641deb76f2b5673e5b64edb4fc1c18d742b74a8
Mihael <mihael.kukec@mev.hr>
2018-12-12 11:25:16 +0100

    Add project files.

---
+++ Vjezba1Pravokutnik/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vjezba1Pravokutnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

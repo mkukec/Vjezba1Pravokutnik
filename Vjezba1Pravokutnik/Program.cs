7047f7b7b50cfb4e14e1cce08798a4ef997db88c
Mihael <mihael.kukec@mev.hr>
2018-12-12 11:28:05 +0100

    Zadatak upisan u Main

--- Vjezba1Pravokutnik/Program.cs
+++ Vjezba1Pravokutnik/Program.cs
@@ -7,6 +7,14 @@ namespace Vjezba1Pravokutnik
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // Izraditi razred pravokutnik koji: (https://hr.wikipedia.org/wiki/Pravokutnik)
+            // 
+            // > može izračunati svoju površinu
+            // > ima konstruktor
+            // > nije ga moguće stvoriti bez zadavanja dimenzija
+            // > ima metode za postavljanje/dohvat vrijednosti
+
         }
     }
 }
